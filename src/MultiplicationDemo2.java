2483835f470d4daa888ccc463334658f1d111a40
tirgani12 <ahmed.abhr6@gmail.com>
2023-04-01 22:13:18 -0400

    multiplication of three numbers done

---
+++ src/MultiplicationDemo2.java
@@ -0,0 +1,9 @@
+public class MultiplicationDemo2 {
+    double x;
+    double y;
+    double z;
+    double multiplication(double x,double y,double z){
+        System.out.println(x*y*z);
+        return x*y*z;
+    }
+}
--- src/multiplicationTest.java
+++ src/multiplicationTest.java
@@ -2,5 +2,7 @@ public class multiplicationTest {
     public static void main(String[] args) {
         MultiplicationDemo md=new MultiplicationDemo();
         md.multipliction(7,6);
+        MultiplicationDemo2 md2=new MultiplicationDemo2();
+        md2.multiplication(5,6,7);
     }
 }
